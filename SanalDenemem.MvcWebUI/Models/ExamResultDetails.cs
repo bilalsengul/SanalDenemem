dbed66a976c7dadb370604975c52da49cb953e6e
yunus şen <yunussen27@hotmail.com>
2020-10-11 12:41:16 +0300

    listeleme tmm

---
+++ SanalDenemem.MvcWebUI/Models/ExamResultDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SanalDenemem.MvcWebUI.Models
+{
+    public class ExamResultDetails
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string  UserName { get; set; }
+        public double Score { get; set; }
+    }
+}
